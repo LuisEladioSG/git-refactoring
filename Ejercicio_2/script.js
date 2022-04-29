1bf09d74e94a4fd310822ef83174ba2e51772d2f
Javier Galvan <javiergalvanpoley@fp.iesromerovargas.com>
2022-04-29 09:19:52 +0200

    Cambio en el nombre de la funcion

--- Ejercicio_2/script.js
+++ Ejercicio_2/script.js
@@ -1,4 +1,4 @@
-function MyFunction1() {
+function CuentaVocales() {
     let array = document.getElementById("texto").value;    
     let vocales = 0;                   
     for (let i = 0; i < 10000; i++) 

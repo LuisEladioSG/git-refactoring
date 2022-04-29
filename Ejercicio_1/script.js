05b09f60f2c466d27a9d97832df309e5e4c9fb37
Javier Galvan <javiergalvanpoley@fp.iesromerovargas.com>
2022-04-29 09:12:07 +0200

    Eliminación de variables y simplificación del html

--- Ejercicio_1/script.js
+++ Ejercicio_1/script.js
@@ -1,11 +1,7 @@
-function Suma2num() {
+function suma2num() {
     let a;
     let b;
-    let c;
-    let resultado;
     a = parseInt(document.getElementById("number1").value);
     b = parseInt(document.getElementById("number2").value);
-    c = a + b;
-    resultado = "El resultado es: " + c;
-    document.getElementById("resultado").innerHTML = resultado;
+    document.getElementById("resultado").innerHTML = "El resultado es: " + (a+b);
 }

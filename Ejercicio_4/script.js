fb514f97573ba52e572118275a8845fb5cfcbd88
Lucas <lucasruizzapata@fp.iesromerovargas.com>
2022-04-29 09:09:16 +0200

    Editado ejercicio 4

--- Ejercicio_4/script.js
+++ Ejercicio_4/script.js
@@ -1,12 +1,10 @@
-function MyFunction1() {
-    var a = document.getElementById("texto").value;
-    var b = parseInt(document.getElementById("numero").value);
-    var total;
-    total = a;
-    for (let i = 0; i < b - 1; i++) {
-        
-        total += a;
-        console.log(total);
+function MultiplicaTexto() {
+    const texto = document.getElementById("texto").value
+    const numero = parseInt(document.getElementById("numero").value);
+    var total
+    for (let i = 0; i < numero - 1; i++) {
+        total += texto;
     }
+    console.log(total);
     window.alert("Aqui tienes: " + total);
 }
